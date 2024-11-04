c9f348fefe23da3e41c379d6a1096fa087f3f634
Tawzib Ahmed Tomal <tawzibahmedtomal@Tawzibs-MacBook-Air.local>
2024-11-04 12:04:18 +0600

    JavaScript Basic Practices

---
+++ index.js
@@ -0,0 +1,107 @@
+// let firstName = "Tomal";
+// console.log(firstName);
+
+// firstName = "Tawzib A.";
+// console.log(firstName);
+
+
+// if (true) {
+//     let lastName = "Ahmed";
+//     console.log(lastName)
+// }
+
+// const lastName = "Ahmed 2";
+
+// console.log(lastName) 
+
+// let name = "Tomal";
+// let age = 21;
+// let isStudent = true;
+
+// console.log(name, age, isStudent);
+
+// var score = 0;
+// let testName = "Ahmed";
+// const gameOver = false;
+
+// console.log(score);
+// var score = 200;
+// console.log(score);
+// testName = "Ahmed";
+
+// console.log(score);
+
+
+// console.log(gameOver);
+
+// Challenge 2: Conditional Reassignment (Using let and const) //
+// let points = 10;
+// const maxPoints = 100;
+
+// console.log(points);
+
+// points = points + 10; // Add 10 to points;
+// console.log("Updated points:", points);
+// // maxPoints = maxPoints + 10;
+
+// console.log(maxPoints);
+
+// let num1 = 5 + 5;
+// let num2 = "4" + 4;
+
+// console.log(num2);
+// console.log(typeof (num2));
+
+// let age = 20;
+// let canVote = age >= 18;
+// console.log("Can vote:", canVote);
+
+// // Check if a person is both old enough to drive and has a driving license.
+// let userName = "Ahmed";
+// let userAge = 24;
+// let hasDrivingLicense = false;
+// let canDrive = userAge >= 18 && hasDrivingLicense;
+
+// console.log( userName + " is old enough and has Driving License \n" + canDrive)
+
+// let firstName = "Tawzib Ahmed";
+// let lastName = "Tomal";
+// let fullName = firstName + " " + lastName;
+// console.log("Full Name is:", fullName)
+
+// let n1 = "5";
+// let n2 = n1 + ("6"  - "5");
+
+// console.log("The value of n2 is:", n2 );
+// console.log(typeof n2)
+
+// Arithmetic Challenge: Write code that calculates the area and perimeter of a rectangle given length and width values. //
+// let height = 12;
+// let width = 6;
+
+// let area = height * width;
+// let perimeter = 2 * (height + width);
+// console.log("Area of the rectangle is:", area);
+// console.log("The Perimeter of the rectangle is:", perimeter);
+
+// Assignment Challenge: Start with a balance of 100, then:
+// 1. Add 50 to the balance.
+// 2. Deduct 30 from the balance.
+// 3. Log the final balance.
+// let startBalance = 100;
+// let finalBalance = (startBalance + 50) - 30;
+// console.log("Final Balance:", finalBalance);
+
+// Check if two different numbers are equal using both == and ===. See if you can spot a difference in behavior.
+// let number1 = "89";
+// let number2 = 89;
+
+// console.log(number1 == number2);
+// console.log(number1 === number2);
+
+// Logical Challenge: Use the && and || operators to check if a number is between 10 and 50 or exactly 100.
+
+// let nmbr = 20;
+
+// console.log(nmbr > 10 && nmbr < 50 || nmbr === 100)
+
