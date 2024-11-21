77c1e6bdae5cb7eee95b61dc0bd03ad397646493
Tawzib Ahmed Tomal <tawzibahmedtomal@Tawzibs-MacBook-Air.local>
2024-11-21 21:19:06 +0600

    New Code Pusj

--- arrays.js
+++ arrays.js
@@ -6,6 +6,9 @@ console.log(fruits.length); // Output: 3
 console.log(fruits[2]);
 
 fruits[1] = "blueberry";
-console.log(fruits[2]);  
+console.log(fruits[2]);
 
 console.log(fruits.sort());
+
+fruits.push("orange");
+console.log(fruits); // ["apple", "banana", "cherry", "orange"]
