fd4c2e2b05c99fbd9e00b07f123418f1f2b54829
Tawzib Ahmed Tomal <tawzibahmedtomal@Tawzibs-MacBook-Air.local>
2025-01-08 12:39:00 +0600

    New Code Push

---
+++ booleans.js
@@ -0,0 +1,18 @@
+let isHungry = true;
+
+if (isHungry) {
+    console.log("Eat pizza!");
+} else {
+    console.log("Don't eat pizza!");
+} 
+
+console.log(10 > 5); // true, because 10 is greater than 5.
+console.log(3 === 7); // false, because 3 is not equal to 7.
+
+
+let myValue = ""; // An empty string is falsy.
+if (myValue) {
+    console.log("This is true.");
+} else {
+    console.log("This is false."); // Output: This is false.
+}
