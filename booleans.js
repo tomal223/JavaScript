cd09a2222688bd160f40f3bbb39870f7433bab11
Tawzib Ahmed Tomal <tawzibahmedtomal@Tawzibs-MacBook-Air.local>
2025-01-09 14:33:48 +0600

    New Code Push

--- booleans.js
+++ booleans.js
@@ -37,26 +37,54 @@
 // }
 
 
+// let isSunny = true;
+// let isWeekend = true;
+
+// if (isSunny && isWeekend) {
+//     console.log("Go for a picnic!");
+// } else {
+//     console.log("Just Take Rest");
+// }
+
+
+// let hasRaincoat = false;
+// let isRaining = true;
+
+// if (hasRaincoat || isRaining) {
+//     console.log("You are ready for the rain!");  
+// }
+
+
+// let isTired = true;
+
+// if (!isTired) {
+//   console.log("Let's keep working!"); // Runs because isTired is false.
+// }
+
+
+// You’ll go outside if it’s sunny AND it’s not raining.
+
 let isSunny = true;
-let isWeekend = true;
+let isRaining = false;
 
-if (isSunny && isWeekend) {
-    console.log("Go for a picnic!");
+if (isSunny && !isRaining) {
+    console.log("It's a perfect weather to go outside");
+}
+
+// let username = ""; // Empty string is falsy.
+// let displayName = username || "Guest";
+// console.log(displayName); // Output: Guest
+
+
+
+let username = "admin";
+let password = "1234";
+
+let inputUsername = "admin";
+let inputPassword = "1234";
+
+if (inputUsername === username && inputPassword === password) {
+  console.log("Login successful!");
 } else {
-    console.log("Just Take Rest");
-}
-
-
-let hasRaincoat = false;
-let isRaining = true;
-
-if (hasRaincoat || isRaining) {
-    console.log("You are ready for the rain!");  
-}
-
-
-let isTired = false;
-
-if (!isTired) {
-  console.log("Let's keep working!"); // Runs because isTired is false.
+  console.log("Invalid username or password.");
 }
