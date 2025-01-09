49aa0877892448729b109f7fa7fef9330b6324aa
Tawzib Ahmed Tomal <tawzibahmedtomal@Tawzibs-MacBook-Air.local>
2025-01-09 13:36:44 +0600

    New Code Push

--- booleans.js
+++ booleans.js
@@ -1,39 +1,62 @@
-let isHungry = true;
+// let isHungry = true;
 
-if (isHungry) {
-    console.log("Eat pizza!");
+// if (isHungry) {
+//     console.log("Eat pizza!");
+// } else {
+//     console.log("Don't eat pizza!");
+// } 
+
+// console.log(10 > 5); // true, because 10 is greater than 5.
+// console.log(3 === 7); // false, because 3 is not equal to 7.
+
+
+// let myValue = ""; // An empty string is falsy.
+// if (myValue) {
+//     console.log("This is true.");
+// } else {
+//     console.log("This is false."); // Output: This is false.
+// }
+
+
+// let num1 = 15;
+// console.log(num1 > 10);
+
+// let str1 = "Tawzib";
+// let str2 = "Ahmed";
+
+// console.log(str1 == str2);
+
+
+// let playerPosition = "Openar";
+
+// if(playerPosition === "Openar") {
+//     console.log("Get ready for opening with the Bat");
+// } else {
+//     console.log("Take some rest and plan your game");
+
+// }
+
+
+let isSunny = true;
+let isWeekend = true;
+
+if (isSunny && isWeekend) {
+    console.log("Go for a picnic!");
 } else {
-    console.log("Don't eat pizza!");
-} 
-
-console.log(10 > 5); // true, because 10 is greater than 5.
-console.log(3 === 7); // false, because 3 is not equal to 7.
-
-
-let myValue = ""; // An empty string is falsy.
-if (myValue) {
-    console.log("This is true.");
-} else {
-    console.log("This is false."); // Output: This is false.
+    console.log("Just Take Rest");
 }
 
 
+let hasRaincoat = false;
+let isRaining = true;
+
+if (hasRaincoat || isRaining) {
+    console.log("You are ready for the rain!");  
+}
 
 
-let num1 = 15;
-console.log(num1 > 10);
+let isTired = false;
 
-let str1 = "Tawzib";
-let str2 = "Ahmed";
-
-console.log(str1 == str2);
-
-
-let playerPosition = "Openar";
-
-if(playerPosition === "Openar") {
-    console.log("Get ready for opening with the Bat");
-} else {
-    console.log("Take some rest and plan your game");
-    
+if (!isTired) {
+  console.log("Let's keep working!"); // Runs because isTired is false.
 }
