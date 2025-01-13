1461719c1aa1eb54630df7fb062a1a6d94924bc1
Tawzib Ahmed Tomal <tawzibahmedtomal@Tawzibs-MacBook-Air.local>
2025-01-14 01:41:58 +0600

    Code Push

---
+++ loops.js
@@ -0,0 +1,51 @@
+// // for loop
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(i + "Hello, world!");
+// }
+
+// let colors = ["red", "blue", "green"];
+
+// for (let i = 0; i < colors.length; i++) {
+//     console.log(colors[i]);
+// }
+
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//     sum += i;
+// }
+// console.log(`Sum: ${sum}`);
+
+
+// for (let n = 0; n < 33; n++) {
+//     console.log(n - 20);
+// }
+
+// let countdown = 5;
+// while (countdown > 0) {
+//     console.log(`Countdown: ${countdown}`);
+//     countdown--;
+// }
+
+// let numbers = [1, 3, 5, 4, 7];
+
+// let i = 0;
+
+// while (i < numbers.length) {
+//     if (numbers[i] % 2 === 0) {
+//         console.log(`First even number: ${numbers[i]}`);
+//         break;
+//     }
+//     i++;   
+// }
+
+
+let i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i <= 5);
+
