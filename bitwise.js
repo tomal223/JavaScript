61c7fa71710885372d0096bafc2aaab8f15d8dbd
tomal223 <towhozibtat223@gmail.com>
2025-02-10 21:54:11 +0600

    New Code Push

---
+++ bitwise.js
@@ -0,0 +1,8 @@
+// console.log(3 & 3);
+// console.log((10).toString(8));
+// console.log((-5).toString(8));
+
+
+console.log(3 | 3);
+console.log((10).toString(8));
+console.log((-5).toString(8));
--- typeConversion.js
+++ typeConversion.js
@@ -1,3 +1,19 @@
 let x = typeof (Number('John'));
 console.log(x);
- 
+
+console.log(Number(""));
+console.log(Number(" "));
+console.log(Number("2.312"));
+console.log( Number(Math.PI));
+console.log(typeof String(Math.PI));
+
+
+let d = new Date();
+
+console.log(Number(d));
+
+
+
+
+
+
