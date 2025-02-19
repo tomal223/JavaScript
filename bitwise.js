7da0a914724536412db8231f88d804455b392f5b
tomal223 <towhozibtat223@gmail.com>
2025-02-19 12:42:08 +0600

    New Code

--- bitwise.js
+++ bitwise.js
@@ -1,8 +1,7 @@
 // console.log(3 & 3);
 // console.log((10).toString(8));
 // console.log((-5).toString(8));
-
-
+ 
 console.log(3 | 3);
 console.log((10).toString(8));
 console.log((-5).toString(8));
